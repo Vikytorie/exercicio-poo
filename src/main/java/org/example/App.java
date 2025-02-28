905589ce455091759b4978fdb92993db7f4907ba
Vickytorie <Vitorgamer13br13@gmail.com>
2025-02-27 21:17:39 -0300

    Salvando branch

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+    }
+    public static boolean verificar(int numero){
+     return numero %2 == 0;
     }
 }
