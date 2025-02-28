0277fdb31600e5d5c681b2bcc1371daa57a09c40
Vickytorie <Vitorgamer13br13@gmail.com>
2025-02-27 21:50:53 -0300

    Salvando progresso

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,17 @@ public class App
 {
     public static void main( String[] args )
     {
+        int num = 5; // Número que queremos calcular o fatorial
+        System.out.println("O fatorial de " + num + " é: " +  );
+    }
 
+    public static fatoriar(int num, int count){
+       int count == num;
+        for (int i = num; i > 0; i--) {
+            num * i = count;
+
+            return num;
+
+        }
     }
 }
