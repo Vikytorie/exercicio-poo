36bbc563fdcf2109bd19db5d8b1d3c85ca48fa82
Vickytorie <Vitorgamer13br13@gmail.com>
2025-02-27 21:26:43 -0300

    Adicionada a função de verificação

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
+    int numero = 8;
 
+    if (verificar(numero)) {
+        System.out.println(numero + " é par");
+    } else {
+        System.out.println(numero + " é impar");
+    }
     }
     public static boolean verificar(int numero){
      return numero %2 == 0;
