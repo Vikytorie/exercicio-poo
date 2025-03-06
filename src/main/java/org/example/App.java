ad4ca318facbc781c12b5f4b9ca1b567722ade89
Vickytorie <Vitorgamer13br13@gmail.com>
2025-03-06 19:40:26 -0300

    Criada a contagem

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,8 +6,21 @@ package org.example;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
+    public static int contarVogais(String str) {
+        int contador = 0; //contador usado de vogais
+        str = str.toLowerCase(); // Converte para minúsculas para facilitar a comparação
 
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public static void main( String[] args ) {
+
+        String texto = "Save Rock and Roll!"; // texto será inserido no método
+        System.out.println("Número de vogais na frase " + texto + ": " + contarVogais(texto));
     }
 }
