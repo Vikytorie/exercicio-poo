ba9d9f0eadc752755bcfbee5ae2492da51e0b632
Vickytorie <Vitorgamer13br13@gmail.com>
2025-03-06 19:28:57 -0300

    media adicionada

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,19 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
+    public static double calcularMedia(double num1, double num2) {
+        return (num1 + num2) / 2.0;
+    }
+
+
     public static void main( String[] args )
     {
+        double numero1 = 39;
+        double numero2 = 14;
 
+        double media = calcularMedia(numero1, numero2); // média chama o metodo calcular, que faz toda a conta
+        System.out.println("A média de " + numero1 + " e " + numero2 + " é: " + media);
     }
 }
